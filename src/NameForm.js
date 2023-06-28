9ff34d56fb5b123395e8194720a98b5db4311a41
Anant Shanbhag <anant.s888@gmail.com>
2023-06-28 08:03:12 +0000

    Update 2 files

--- src/NameForm.js
+++ src/NameForm.js
@@ -36,28 +36,88 @@ function NameForm() {
 
   return (
     <div style={{ display: "flex", height: "80vh" }}>
-      <div style={{ flex: 1, border: "1px solid black", paddingLeft: "5px" }}>
-        <label>Enter Name: </label>
-        <input type="text" value={name} onChange={handleNameChange} />
-        <br />
-        <label>Digits: {nameToDigits}</label>
-        <br />
-        <label>Total: {totalOfDigits > 0 && totalOfDigits}</label>
-        <br />
-        <br />
-        <label>Select birthdate: </label>
-        <input type="date" value={date} onChange={handleDateChange} />
-        <br />
-        <label>Birth Number: {bn > 0 && bn}</label>
-        <br />
-        <label>Destiny Number: {dn > 0 && dn}</label>
-        <br />
-        <label>Lucky Numbers: {luckyNumbers.join(", ")}</label>
+      <div
+        style={{
+          flex: 1,
+          border: "1px solid black",
+          padding: "5px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+        }}
+      >
+        <label
+          style={{ marginBottom: "5px", fontSize: "16px", fontWeight: "bold" }}
+        >
+          Enter Name:
+        </label>
+        <input
+          type="text"
+          value={name}
+          onChange={handleNameChange}
+          placeholder={"name"}
+          style={{
+            marginBottom: "5px",
+            padding: "5px",
+            fontSize: "14px",
+            borderRadius: "5px",
+            border: "1px solid #ccc",
+          }}
+        />
+        <label
+          style={{ marginBottom: "5px", fontSize: "16px", fontWeight: "bold" }}
+        >
+          Digits: {nameToDigits}
+        </label>
+        <label
+          style={{ marginBottom: "10px", fontSize: "16px", fontWeight: "bold" }}
+        >
+          Total: {totalOfDigits > 0 && totalOfDigits}
+        </label>
+        <label
+          style={{
+            marginTop: "20px",
+            marginBottom: "5px",
+            fontSize: "16px",
+            fontWeight: "bold",
+          }}
+        >
+          Select birthdate:
+        </label>
+        <input
+          type="date"
+          value={date}
+          onChange={handleDateChange}
+          style={{
+            marginBottom: "5px",
+            padding: "5px",
+            fontSize: "14px",
+            borderRadius: "5px",
+            border: "1px solid #ccc",
+          }}
+        />
+        <label
+          style={{ marginBottom: "5px", fontSize: "16px", fontWeight: "bold" }}
+        >
+          Birth Number: {bn > 0 && bn}
+        </label>
+        <label
+          style={{ marginBottom: "5px", fontSize: "16px", fontWeight: "bold" }}
+        >
+          Destiny Number: {dn > 0 && dn}
+        </label>
+        <label
+          style={{ marginBottom: "5px", fontSize: "16px", fontWeight: "bold" }}
+        >
+          Lucky Numbers: {luckyNumbers.join(", ")}
+        </label>
       </div>
       <div
         style={{
           border: "1px solid black",
           overflowY: "auto",
+          padding: "10px",
         }}
       >
         <DigitLettersView nameLetterMap={nameLetterMap} />
